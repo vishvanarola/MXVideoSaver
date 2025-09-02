34a85539ef9f154c05c6cbf6e4d6d35a557facf7
vishva <vishvanarola02@gmail.com>
2025-09-02 11:31:49 +0530

    Project initialise

---
+++ MXVideoSaver/MXVideoSaver/MXVideoSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MXVideoSaverApp.swift
+//  MXVideoSaver
+//
+//  Created by vishva narola on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MXVideoSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
